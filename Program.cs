f8c35e4d4239a22d982079ebccd721c006874c8e
Caslo1 <71670417+Caslo1@users.noreply.github.com>
2020-09-22 00:32:21 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Caslo.ForWhileForeach
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for(int Value = 0; Value <= 15; Value++)
+            {
+                Console.WriteLine(Value);
+            }
+            Console.ReadLine();
+        }
+    }
+}
