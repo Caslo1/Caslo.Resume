5165f9ebee0861a61a51d4454d7c02d60b96aad3
Caslo1 <71670417+Caslo1@users.noreply.github.com>
2020-09-23 13:52:48 +0300

    Learn C#
    Classes

---
+++ Classes.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace Caslo.Classes
+{
+
+    class Classes
+    {
+        static void Main(string[] args)
+        {
+            Person character = new Person();
+
+            Console.WriteLine("Введите расу персонажа");
+            character.Race = Console.ReadLine();
+
+            Console.WriteLine("Введите роль персонажа");
+            character.Role = Console.ReadLine();
+
+            Console.WriteLine("Введите цвет персонажа");
+            character.Color = Console.ReadLine();
+
+            Console.WriteLine("Введите рост персонажа от 150 до 210 см");
+            character.Height = int.Parse(Console.ReadLine());
+
+            if(character.Height < 150 && character.Height >210)
+            {
+
+            }
+            else
+            {
+                Console.WriteLine("Повторите попытку");
+                Console.ReadLine();
+                return;
+            }
+
+            Console.WriteLine(character.getChatacter);
+            Console.ReadLine();
+    }
+    }
+}
---
+++ Person.cs
@@ -0,0 +1,23 @@
+﻿namespace Caslo.Classes
+{
+    class Person
+    {
+        public string Race { get; set; }
+        public string Role { get; set; }
+        public string Color { get; set; }
+        public int Height { get; set; }
+
+        public Person()
+        {
+
+        }
+
+        public string getChatacter
+        {
+            get
+            {
+                return $"Раса: {Race}, Роль: {Role}, Цвет: {Color}, Рост: {Height}.";
+            }
+        }
+    }
+}
