cb1328a662089d05857a7b2a57c589aed3002fd8
Caslo1 <71670417+Caslo1@users.noreply.github.com>
2020-09-24 18:41:31 +0300

    Learn C#
    test

---
+++ Factori.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Caslo.Factori
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int factor = int.Parse(Console.ReadLine());
+            int result = 0;
+
+            if(factor == 0)
+            {
+
+                Console.WriteLine("Повторите попытку");
+                Console.ReadLine();
+                return;
+
+            }
+
+            if (factor == 1 || factor == 2)
+            {
+                result = 1;
+            }
+            else
+            {
+                result = (factor - 1) + (factor - 2);
+            }
+            Console.WriteLine(result);
+            Console.ReadLine();
+        }
+    }
+}
