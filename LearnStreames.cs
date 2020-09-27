52d48556d51ad4719e8f99aecd74f4a9d06eca0d
Caslo1 <71670417+Caslo1@users.noreply.github.com>
2020-09-27 15:28:52 +0300

    Learn C#
    Lesson Stream

---
+++ LearnStreames.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.IO;
+using System.Text;
+
+
+namespace Caslo.FileStream
+{
+    class LearnStreames
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите имя персонажа");
+            var character = Console.ReadLine();
+
+            using (var filewrite = new StreamWriter("LearnStream.txt", true, Encoding.UTF8))
+            {
+                filewrite.Write($"Hello {character}");
+                filewrite.WriteLine(" Это ваш первый персонаж");
+            }
+
+            using (var fileread = new StreamReader("LearnStream.txt", Encoding.UTF8))
+            {
+                var frRead = fileread.ReadToEnd();
+                Console.Write(frRead);
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
