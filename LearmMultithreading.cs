9713c96ded5a8c91531a11b19ba4f052c9e093ee
Caslo1 <71670417+Caslo1@users.noreply.github.com>
2020-09-27 22:57:30 +0300

    Learn C#
    Learn Async and Thread

---
+++ LearmMultithreading.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Threading;
+
+namespace Caslo.Multithreading
+{
+    class LearmMultithreading
+    {
+        static void Main(string[] args)
+        {
+            Thread firstCharacter = new Thread(new ThreadStart(lvlFirst));
+            firstCharacter.Start();
+
+            Thread secondCharacter = new Thread(new ParameterizedThreadStart(lvlSecond));
+            secondCharacter.Start(1000);
+
+            Console.ReadLine();
+        }
+
+        static void lvlFirst()
+        {
+            int expCharacter = 0;
+            for (int exp = 0; exp < 1000; exp++)
+            {
+                expCharacter++;
+                if(expCharacter % 10 == 0)
+                {
+                    Console.WriteLine("Первый игрок получил опыт");
+                }
+            }
+        }
+
+        static void lvlSecond(object numeral)
+        {
+            int expCharacter = 0;
+            for (int exp = 0; exp < (int)numeral; exp++)
+            {
+                expCharacter++;
+                if (expCharacter % 10 == 0)
+                {
+                    Console.WriteLine("Второй игрок получил опыт");
+                }
+            }
+        }
+    }
+}
---
+++ LearnAsynchrony.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace Caslo.Asynchrony
+{
+    class LearnAsynchrony
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Вы начали уровень");
+
+            lvlupAsync();
+
+            for(int unit = 0; unit < 20; unit++)
+            {
+                Console.WriteLine($"Вы победили {unit}");
+            }
+            Console.WriteLine("Вы победили всех врагов");
+            Console.ReadLine();
+        }
+
+        static async Task lvlupAsync()
+        {
+            Console.WriteLine("Вы начинаете получать опыт");
+            await Task.Run(() => Experience());
+            Console.WriteLine("Поздравляю вас с повышением уровня");
+        }
+        static void Experience()
+        {
+            for(int exp = 0; exp < 20; exp++ )
+            {
+                Console.WriteLine($"Ваш текущий опыт {exp}");
+            }
+
+        }
+    }
+}
