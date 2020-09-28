d9722ccc1b0c9be70bb00f8e99934ebc3b5706f2
Caslo1 <71670417+Caslo1@users.noreply.github.com>
2020-09-28 23:26:59 +0300

    Learn C# TCP

---
+++ ServerTCP.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+namespace Caslo.LearnTCP
+{
+    class ServerTCP
+    {
+        static void Main(string[] args)
+        {
+            const string ip = "127.0.0.1";
+            const int port = 1321;
+
+            var tcpEndPoint = new IPEndPoint(IPAddress.Parse(ip), port);
+
+            var tcpSocket = new Socket(AddressFamily.InterNetwork, SocketType.Stream , ProtocolType.Tcp);
+            tcpSocket.Bind(tcpEndPoint);
+            tcpSocket.Listen(4);
+
+            while(true)
+            {
+                var listener = tcpSocket.Accept();
+                var buffer = new byte[256];
+                var size = 0;
+                var date = new StringBuilder();
+
+                do
+                {
+                    size = listener.Receive(buffer);
+                    date.Append(Encoding.UTF8.GetString(buffer, 0, size));
+                }
+                while (listener.Available > 0);
+
+                Console.WriteLine(date);
+
+                listener.Send(Encoding.UTF8.GetBytes("Вы отправили сообщение пользователю"));
+
+                listener.Shutdown(SocketShutdown.Both);
+                listener.Close();
+            }
+        }
+    }
+}
---
+++ UserServerTCP.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+namespace Caslo.UserTCP
+{
+    class UserServerTCP
+    {
+        static void Main(string[] args)
+        {
+            const string ip = "127.0.0.1";
+            const int port = 1321;
+
+            var tcpEndPoint = new IPEndPoint(IPAddress.Parse(ip), port);
+
+            var tcpSocket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+
+            Console.WriteLine("Введите свой псевдоним");
+            var name = Console.ReadLine();
+
+            Console.WriteLine("Введите сообщение: ");
+            var message = Console.ReadLine();
+
+            var date = Encoding.UTF8.GetBytes($"{name}: {message}");
+
+            tcpSocket.Connect(tcpEndPoint);
+            tcpSocket.Send(date);
+
+            var buffer = new byte[256];
+            var size = 0;
+            var answer = new StringBuilder();
+
+            do
+            {
+                size = tcpSocket.Receive(buffer);
+                answer.Append(Encoding.UTF8.GetString(buffer, 0, size));
+            }
+            while (tcpSocket.Available > 0);
+
+            Console.WriteLine(answer.ToString());
+
+            tcpSocket.Shutdown(SocketShutdown.Both);
+            tcpSocket.Close();
+
+            Console.ReadLine();
+        }
+    }
+}
