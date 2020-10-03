eea31f559bfe2e778e09592ef2b28aad1b4dc119
Caslo1 <71670417+Caslo1@users.noreply.github.com>
2020-10-03 19:29:10 +0300

    Learn C# AnonMethandLambda
    Anonymous Method and Lambda Expressions

---
+++ LearnAnonymMethodandLymbda.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace Caslo.AnonymMethodAndLambdaExpressions
+{
+    class LearnAnonymMethodandLymbda
+    {
+        delegate int lvlup(int shortage);
+        static void Main(string[] args)
+        {
+            var exp = 1000;
+            var unit = 250;
+
+            lvlup lvlup1 = delegate (int shortage)
+            {
+                shortage = exp - unit;
+                Console.WriteLine($"Вам не хватает {shortage} до поднятия уровня");
+                return shortage;
+            };
+
+            lvlup1(750);
+
+            lvlup lvlup2 = (shortage) =>
+            {
+                shortage = exp - unit * 2;
+                Console.WriteLine($"Вам не хватает {shortage} до поднятия уровня");
+                return shortage;
+            };
+            lvlup2(500);
+
+            Console.ReadLine();
+        }
+    }
+}
