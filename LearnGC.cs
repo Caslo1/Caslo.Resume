cbb75745025c9bcc7588c0ed7eda675398c1d517
Caslo1 <71670417+Caslo1@users.noreply.github.com>
2020-10-06 16:03:12 +0300

    Learn C# GC

---
+++ LearnGC.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace Caslo.GCs
+{
+    class LearnGC
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(GC.GetTotalMemory(false));
+
+            var value = GC.GetTotalMemory(true);
+
+            for (var i = 0; i < 10000; i++)
+            {
+                var Character = (object)i;
+                int list = (int)Character;
+            }
+
+            var quantity = 0;
+            Console.WriteLine(GC.CollectionCount(quantity));
+
+            Console.WriteLine(GC.GetTotalMemory(false));
+
+            GC.Collect();
+
+            Console.WriteLine(GC.GetTotalMemory(false));
+
+            Console.ReadLine();
+
+        }
+    }
+}
